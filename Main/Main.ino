11065bc0f3f8ec8400643fb30f12a840522b891f
Daniel <danielgrohmann21@gmail.com>
2024-06-04 00:07:08 +0200

    Main program has been deffined and is ready for testing on a fysical arduino platform

--- Main/Main.ino
+++ Main/Main.ino
@@ -2,35 +2,37 @@
 LiquidCrystal lcd(7, 6, 5, 4, 3, 2);
 
 //PIN ASIGMENTS WILL BE CHANGED LATER
-// Buttons pins asignations
+//Buttons pins asignations
 const int feedingButtonPin = 8;
-const int emptyButtonPin = 9;
+const int emptyBoxPin = 9;
 const int bbSensorPin = 10;
 const int loadingModePin = 11;
 const int displayModePin = 12;
-// encoder adn radio mode switches will be added later
+const int encoderDepressPin = 13;
+// encoder and radio mode switches will be added later
 
 //Actuators pin asigments
-const int motor = 13;
+const int motor = 16;
 
 //Variables reading set up
-int feedingButton = 0;
-int emptyButton = 0;
-int bbSensor = 0;
-int loadingMode = 0;
-int displayMode = 0;
+int feedingButtonState = 0;
+int emptyBoxState = 0;
+int bbSensorState = 0;
+int loadingModeState = 0;
+int displayModeState = 0;
+int encoderDepressState = 0;
 
 //Utility variables
-int lBS = 0;
+int lastbbSensorState = 0;
 
 void setup() {
   // INITIAL PARAMETERS
 
   // Button input/output set
   pinMode(feedingButtonPin, INPUT);
-  pinMode(emptyButtonPin, INPUT);
+  pinMode(emptyBoxPin, INPUT);
   pinMode(bbSensorPin, INPUT);
-  pinMode(loadingModePinv, INPUT);
+  pinMode(loadingModePin, INPUT);
   pinMode(displayModePin, INPUT);
   pinMode(motor, OUTPUT);
 
@@ -45,31 +47,60 @@ void loop() {
   lcd.setCursor(0, 0);
   lcd.print("Ativado");
 
-  //Variable Initialization
+  //Button variable initialization
+  feedingButtonState = digitalRead(feedingButtonPin);
+  emptyBoxState = digitalRead(emptyBoxPin);
+  bbSensorState = digitalRead(bbSensorPin);
+  loadingModeState = digitalRead(loadingModePin);
+  displayModeState = digitalRead(displayModePin);
+  encoderDepressState = digitalRead(encoderDepressPin);
+
+  //Independent variable Initialization
   int bbCount = 0;
   
-  //Main program
-  while 
+  //MAIN PROGRAM
+  
+  
+  while (emptyBoxState == HIGH){
+    info();
+    while (feedingButtonState == HIGH){
+      feed();
+    }
+  }
+  //Empty deposit ALERT
+  else{
+    lcd.setCursor(0, 0);
+    lcd.print("Deposito Vacio");
+    lcd.setCursor(0, 1);
+    lcd.print("Dial + tubo");
+    if ((feedingButtonState == HIGH) && (encoderDepressState == HIGH)){
+      feed();
+    }
+  }
 }
 
-void main(){
+void info(){
   lcd.setCursor(0, 0);
+  lcd.print("No loading limit");
+  lcd.setCursor(0, 1);
   lcd.print("Fusil"); //Later will be changed by profile selections
-  lcd.setCursor(0,1);
+  lcd.setCursor(0, 13);
+  lcd.print("30"); //Later will be changed by profile selections
+  lcd.setCursor(13,1);
   lcd.print(bbCounter());
 }
 
 //This method counts the amount of bbs that have been feed
 int bbCounter(){
-  cBS = digitalRead(bbSensorPin);
+  bbSensorState = digitalRead(bbSensorPin);
   //Only counts the times the sensor has gone from of to on
-  if (lBS != cBS){
-    if (cBS == HIGH){
+  if (lastbbSensorState != bbSensorState){
+    if (bbSensorState == HIGH){
       bbCount++;
-      lBS = cBS;
+      lastbbSensorState = bbSensorState;
     }
     else{
-      lBS = cBS;
+      lastbbSensorState = bbSensorState;
     }
   }
   return bbCount;
