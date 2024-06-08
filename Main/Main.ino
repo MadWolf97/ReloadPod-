2f058c13564d58c4f7f0cc42a9370d9fd9633881
Daniel <danielgrohmann21@gmail.com>
2024-06-08 20:03:05 +0200

    sOME DEBUGGING DONE

--- Main/Main.ino
+++ Main/Main.ino
@@ -1,7 +1,27 @@
+//LIBRARIES
 #include <LiquidCrystal.h>
-LiquidCrystal lcd(7, 6, 5, 4, 3, 2);
+/*Will be implemented with TackNet
+#include <RF24Network.h>
+#include <RF24.h>
+#include <SPI.h>
+*/
 
 //PIN ASIGMENTS MIGHT BE CHANGED LATER
+  //Screen pins asignations
+  LiquidCrystal lcd(7, 6, 5, 4, 3, 2);
+  
+  /*Will be implemented with TackNet
+  //Radio pins asignation
+  RF24 radio(10, 9);               // nRF24L01 (CE,CSN)
+  RF24Network network(radio);      // Include the radio in the network
+  //Radio Buttons asignation
+  const uint16_t this_node = 02;   // Address of our node in Octal format ( 04,031, etc)
+  const uint16_t master00 = 00;    // Address of the other node in Octal format
+  //Radio variable inicialization
+  const unsigned long interval = 10;  //ms  // How often to send data to the other unit
+  unsigned long last_sent;            // When did we last send?
+  */
+  
   //Buttons pins asignations
   const int emptyBoxPin = 8;
   const int feedingButtonPin =14;
@@ -16,7 +36,6 @@ LiquidCrystal lcd(7, 6, 5, 4, 3, 2);
 
   //Actuators pin asigments
   const int motor = 19;
-
 
 //GOLBAL VARIABLE INITIALLIZATION
   //Variables reading set up
@@ -41,7 +60,7 @@ LiquidCrystal lcd(7, 6, 5, 4, 3, 2);
   bool magLimitation;
   bool aviability;
 
-  // INITIAL PARAMETERS
+  // INITIAL PROFILE PARAMETERS
   //Preset Profiles   (profileName, bbLimit, tokenNeeded)
   /*
   ("Pistola",14,1);
@@ -58,9 +77,6 @@ LiquidCrystal lcd(7, 6, 5, 4, 3, 2);
 
 //THIS LOADS ALL SETTINGS AND MESAGES BEFORE GAME USEAGE
 void setup() {
-  //test
-  token = 0;
-
   // Button input/output set
   pinMode(emptyBoxPin, INPUT);
   pinMode(feedingButtonPin, INPUT);
@@ -100,6 +116,12 @@ void setup() {
 
   //Slaved mode: TacNet ON
   else{
+    /*Will be implemented with TackNet
+    SPI.begin();
+    radio.begin();
+    network.begin(90, this_node);  //(channel, node address)
+    radio.setDataRate(RF24_2MBPS);
+    */
     magLimitation = true;
     lcd.setCursor(0, 0);
     lcd.print("MODO DE CONTROL");
@@ -118,30 +140,18 @@ void setup() {
   */
 }
 
-//This function actualices ONLY the button states needed during refill
-void buttonStateUpdateReduced (){
-  emptyBoxState = digitalRead(emptyBoxPin);
-  feedingButtonState = digitalRead(feedingButtonPin);
-  bbSensorState = digitalRead(bbSensorPin);
-}
-
-//This function actualices ALL the button states for regular operation
-void buttonStateUpdate (){
-  buttonStateUpdateReduced ();
-  /* will be added with profiles implementation
-  encoderUpState = digitalRead(encoderUpPin);      
-  encoderDownState = digitalRead(encoderDownPin);   
-  */
-  encoderDepressState = digitalRead(encoderDepressPin);
-}
-  
 //MAIN LOOP
 void loop() {
-  buttonStateUpdate();
+  //LOOP VARIABLES INICIALIZATION
+  bool lastAlert = false;
+  bool lastRefill = false;
 
+  //BUTTONS UPDATE
+  buttonStateUpdate();
+  
   //PROFILE AND TOKEN ADQUISITION
-  /*Will be added with Taknet
-  getTokens from Main unit
+  /*Will be imlemented with TackNet
+  token = tokenUpdate();
   */
 
   //Temporal try, will be deleted with Profiles implementation
@@ -155,32 +165,43 @@ void loop() {
   */
 
   //MAIN PROGRAM
-  if(emptyBoxState == LOW){
+  while (emptyBoxState == LOW){
     emptyAlert();
+    lastAlert = true;
   }
 
-  else {
+  //Only gets displayed if there was an empty box alert
+  if (lastAlert == true){
+    refillSuccess();
+    delay (3000);
+  }
+
+  while (emptyBoxState == HIGH){
+    buttonStateUpdate();
     bbCount = 0;
     infoDisplay();
     if (aviability == true){ 
       if (feedingButtonState == HIGH){
+        lastRefill = true;
+        bbCounter();
         if (magLimitation == true){
-          //Will be compleetly be changed with TackNet
+          //Will be changed with TackNet
           token = token - tokenNeeded;      
-          /*
-          TackNet comunication will be added latter
-          comunicate the amount of tokens consumed to Main Unit
+          /*Will be implemented with TackNet
+          tokenConsumption(token = token - tokenNeeded);
           */
           }
           if (magLimitation == false){
-            //This is only used for getting the statitics needed for equilibrating games
+            /* This is only used for getting the statitics 
+            needed for equilibrating future games */
             tokenCounter();
           }
-        bbCounter();
         feedCut();
-        //Delay in order to see the bbCount
-        delay(2000);
       }
+    }
+    //Delay in order to see the last bbCount
+    if (lastRefill == true){
+      delay(3000);
     }
   }
   //Precaution if some of the cicles gets interrupted
@@ -213,6 +234,7 @@ void infoDisplay(){
 
 // Empty deposit ALERT and REFILLING
 void emptyAlert(){
+  buttonStateUpdateReduced();
   lcd.setCursor(0, 0);
   lcd.print("DEPOSITO VACIO  ");
   lcd.setCursor(0, 1);
@@ -225,8 +247,16 @@ void emptyAlert(){
   feedCut();
 }
 
+//This informs that the refilling process was succesfull
+void refillSuccess(){
+  lcd.setCursor(0, 0);
+  lcd.print("     RECARGA    ");
+  lcd.setCursor(0, 1);
+  lcd.print("   FINALIZADA   ");
+}
+
 //This method feeds bbs, counts and displays the bbs that have been feed
-void bbCounter (){
+void bbCounter(){
   //Only counts the times the sensor has gone from OFF to ON
   while (bbCount < bbLimit && feedingButtonState == HIGH && emptyBoxState == HIGH){
     feed();
@@ -239,15 +269,15 @@ void bbCounter (){
         lastbbSensorState = bbSensorState;
       }
     }
-    lcd.setCursor(9,1);
-    lcd.print(numFormat(bbCount));
-    buttonStateUpdateReduced(); 
+      lcd.setCursor(9,1);
+      lcd.print(numFormat(bbCount));
+      buttonStateUpdateReduced(); 
   }
   feedCut();
 }
 
 //this method shows how many mags you can reload
-int magCounter (){
+int magCounter(){
   int mags = token / tokenNeeded;
   if (mags > 0){
       aviability = true;
@@ -264,7 +294,44 @@ int tokenCounter(){
   return token;
 }
 
+/*Will be implemented with TackNet
+//This method gets the information from the main unit
+int tokenUpdate(){ 
+  network.update();
+  network.read(avaliableTokens); // Read the incoming data
+  return avaliableTokens;
+}
+
+//This method comunicates the tokens consumed by the last refill to the main unit
+void tokenConsumption (int token){
+  network.update();
+  unsigned long now = millis();
+  if (now - last_sent >= interval) {   // If it's time to send a data, send it!
+    last_sent = now;
+    RF24NetworkHeader header(master00);   // (Address where the data is going)
+    network.write(token); // Send the data
+  }
+}
+*/
+
 //THE FUNCTIONS BELOW ARE FOR CONVENIENCE
+//This function actualices ONLY the button states needed during refill
+void buttonStateUpdateReduced (){
+  emptyBoxState = digitalRead(emptyBoxPin);
+  feedingButtonState = digitalRead(feedingButtonPin);
+  bbSensorState = digitalRead(bbSensorPin);
+  encoderDepressState = digitalRead(encoderDepressPin);
+}
+
+//This function actualices ALL the button states for regular operation
+void buttonStateUpdate (){
+  buttonStateUpdateReduced ();
+  /* will be added with profiles implementation
+  encoderUpState = digitalRead(encoderUpPin);      
+  encoderDownState = digitalRead(encoderDownPin);   
+  */
+}
+
 //This method formats the numbers returned by counting methods
 String numFormat (int number){
   if (number < 10){
