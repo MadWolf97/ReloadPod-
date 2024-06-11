277d435cac0e1a513a690c12b182ae2233145908
Daniel <danielgrohmann21@gmail.com>
2024-06-11 19:23:43 +0200

    Discarded the reubication of some methods to cpp file

--- Main/Profiles.cpp
+++ Main/Profiles.cpp
@@ -6,28 +6,3 @@
 void Profiles::changebbLimit(){
 
 }
-
-//this method feeds the amount of bbs specified by the profile
-void Profiles::bbFeeder(int bbLimit){
-    int bbCount = 0;
-    while (bbCount < this.bbLimit){
-        buttonStateUpdateReload();
-        feed();
-        lcd.setCursor(9,1);
-        lcd.print(numFormat(bbCounter()));
-    }
-}
-
-//This method counts the bbs feeded and returns its value
-int Profiles::bbCounter(int bbSensorState, int lastbbSensorState, int bbCount){
-    if (lastbbSensorState != bbSensorState){
-      if (bbSensorState == HIGH){
-        bbCount++;
-        lastbbSensorState = bbSensorState;
-      }
-      else{
-        lastbbSensorState = bbSensorState;
-      }
-    }
-    return bbCount;
-}
